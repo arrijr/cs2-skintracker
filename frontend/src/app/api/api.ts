dca79f00bb55b5222915df0cc4e6cd03edc04f63
Arthur R <arthur_richelhof@yahoo.de>
2025-08-06 17:52:23 +0200

    ok

--- frontend/src/app/api/api.ts
+++ frontend/src/app/api/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL; 
+const BASE_URL = "https://cs2-skintracker.onrender.com/api/v1";
 console.log("Loaded BASE_URL:", BASE_URL);
 
 function getToken() {
