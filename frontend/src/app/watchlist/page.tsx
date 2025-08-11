d33810e65084912404443eab482d7162d4442287
Arthur R <arthur_richelhof@yahoo.de>
2025-08-11 11:01:33 +0200

    update

--- frontend/src/app/watchlist/page.tsx
+++ frontend/src/app/watchlist/page.tsx
@@ -1,5 +1,9 @@
+"use client";
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 import WatchlistPage from "../components/WatchlistPage";
 
 export default function Page() {
   return <WatchlistPage />;
-}
+  }
